3b68972ceb388747d9a563342aaeebd534765010
SavindiRashmika <rashmikawadanambi>
2024-01-21 16:40:51 +0530

    updated

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/BackEnd/dto/ItemDTO.java
@@ -0,0 +1,4 @@
+package lk.ijse.gdse66.BackEnd.dto;
+
+public class ItemDTO {
+}
--- BackEnd/src/main/java/lk/ijse/gdse66/BackEnd/servlet/CustomerServlet.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/BackEnd/servlet/CustomerServlet.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse66.BackEnd.servlet;
 
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
@@ -14,6 +15,16 @@ public class CustomerServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         System.out.println("save");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("dopost");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("do delete");
     }
 
     public void destroy() {
